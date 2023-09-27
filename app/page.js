c87fb108ee2cccdfef8ffb408ee17fcf3da1b37f
marex980 <marex980@gmail.com>
2023-09-27 23:57:14 +0200

    hero section

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { royale, logirent, poppins } from '../styles/fonts';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="home flex min-h-screen flex-col items-center justify-between p-7">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -31,35 +31,69 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 2.5,
-            opacity: 1,
-            transition: {
-              delay: 0.1,
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
             },
-          },
-        }}
-      >
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-      </motion.div>
+            visible: {
+              scale: 2.5,
+              opacity: 1,
+              transition: {
+                delay: 0.1,
+              },
+            },
+          }}
+        >
+          {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={180}
+              height={37}
+              priority
+            />
+          </div> */}
+          <div className="hero__text flex">
+            <div className="hero__title text-light fade-in">
+              <h1>
+                <span className={`${royale.className} text-2xl`}>DESiGNER</span>
+                <span className={`${royale.className} text-2xl`}> &</span>
+                <span className={`${royale.className} text-2xl`}>
+                  DevelOper
+                </span>
+              </h1>
+            </div>
+            <div className="hero__description">
+              <p className="hero__description--text fs-700 ff-sans--sec fade-in">
+                <span className="text__gradient text__gradient--fast fw-700">
+                  FAST
+                </span>
+                ,{' '}
+                <span className="text__gradient text__gradient--safe fw-700">
+                  SAFE
+                </span>{' '}
+                anD{' '}
+                <span className="text__gradient text__gradient--cheap fw-700">
+                  CHEAP
+                </span>
+                <span className="d-block">
+                  CUSTOM MADE{' '}
+                  <span className="hero__description--website ff-sans--sec">
+                    webSiteS!
+                  </span>
+                </span>
+              </p>
+            </div>
+          </div>
+        </motion.div>
+        <div></div>
+      </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
