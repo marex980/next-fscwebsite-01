8ea5cd4ef2df8c0372c7a02b20aca2251159b4cc
marex980 <69250892+marex980@users.noreply.github.com>
2023-09-27 13:32:07 +0200

    fonts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,29 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const royal = localFont({
+  src: './fonts/ROYALERegular.woff2',
+  display: 'swap',
+  weight: 'normal',
+  style: 'normal',
+})
+
+const logirent = localFont({
+  src: './fonts/Logirent-Bold.ttf',
+  display: 'swap',
+  weight: 'bold',
+  style: 'normal',
+})
+
+const poppins = localFont({
+  src: './fonts/Poppins-Regular.ttf',
+  display: 'swap',
+  weight: 'normal',
+  style: 'normal',
+})
 
 export const metadata = {
   title: 'FSC | Creating fastest, safest and cheapest websites on the world!',
@@ -11,7 +33,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${royal.className} ${logirent.className} ${poppins.className}`}>{children}</body>
     </html>
   )
 }
+
