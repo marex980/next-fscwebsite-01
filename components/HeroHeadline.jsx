f616fd91e9b6b408a3bad2051844eb567153c81b
marex980 <69250892+marex980@users.noreply.github.com>
2023-10-02 16:20:58 +0200

    d

--- components/HeroHeadline.jsx
+++ components/HeroHeadline.jsx
@@ -14,7 +14,7 @@ const HeroHeadline = ({
 }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
-      <span>{titleRoyale}</span> <span>{titleRoyaleSeparator}</span>{" "}
+      <span>{titleRoyale} <span className="inline-block">{titleRoyaleSeparator}</span>{" "} </span> 
       <span className="block">{titleLogirent}</span>
     </h1>
     <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
