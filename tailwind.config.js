17ea78aa7a0b3d3ffb4f95a5d0e6caa6ed06ccbb
marex980 <69250892+marex980@users.noreply.github.com>
2024-05-23 14:40:32 +0200

    text size modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       transitionTimingFunction: {
         'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
       },
+      fontSize: {
+        xxl: "clamp(2rem, 0.23529411764705888rem + 11.76470588235294vw, 8rem)",
+      },
     },
   },
   plugins: [],
